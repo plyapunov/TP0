b8539bf1e042b06579ff840c025502985a0f2c98
plyapunov <plyapunov@yahoo.com.ar>
2015-04-05 22:34:03 -0300

    TP0
    Trabajo Practico n°0
    Lyapunov Pylyp

---
+++ Hola Mundo.cpp
@@ -0,0 +1,13 @@
+//============================================================================
+// Name        : Hola.cpp
+// Author      : Lyapunov Pylyp
+// Version     :
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hola Mundo!" << endl;
+	return 0;
+}
